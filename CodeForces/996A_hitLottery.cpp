30c4e477ab2bb6b1e7b7c3e1026889242705b8fa
ARSHHARIT27 <harit27205@gmail.com>
2026-06-14 18:35:44 +0530

    solved the hit the lottery problem used greedy approach

---
+++ CodeForces/996A_hitLottery.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int n;
+   int count=0;
+   cin>>n;
+   while(n!=0){
+    if(n>=100){
+        count+=n/100;
+        n=n%100;
+    }else if(n>=20 && n<100){
+        count+=n/20;
+        n=n%20;    
+    }else if(n>=10 && n<20){
+        count+=n/10;
+        n=n%10;
+    }else if(n>=5){
+        count+=n/5;
+        n=n%5;
+    }else if(n>=1){
+        count+=n/1;
+        n=n%1;
+    }
+   }
+   cout<<count;
+   return 0;
+}
