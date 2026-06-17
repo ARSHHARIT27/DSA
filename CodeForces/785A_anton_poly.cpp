816dee9d5aefb69d2f29631cc5df951a69654fd5
ARSHHARIT27 <harit27205@gmail.com>
2026-06-17 17:12:14 +0530

    solved prob containning string and its implementation

---
+++ CodeForces/785A_anton_poly.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+   int n;
+   int count=0;
+   cin>>n;
+   for(int i=1;i<=n;i++){
+    string s;
+    cin>>s;
+    if(s=="Tetrahedron"){
+        count+=4;
+    }else if(s=="Cube"){
+        count+=6;
+    }else if(s=="Octahedron"){
+        count+=8;
+   }else if(s=="Dodecahedron"){
+        count+=12;
+   }else if(s=="Icosahedron"){
+    count+=20;
+   }
+}
+    cout<<count;
+   return 0;
+}
