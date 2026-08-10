25afabf77e4bfa4250de7bc3feb8c28f190a534a
ARSHHARIT27 <harit27205@gmail.com>
2026-08-10 22:23:19 +0530

    solved

---
+++ CodeForces/1772A_aplusb.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        int a;
+        int b;
+        a=s[0]-'0';
+        b=s[2]-'0';
+        cout<<a+b<<endl;
+    }
+    return 0;
+}
